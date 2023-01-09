d24ca19005ce6e24af0442d5fd0264cf306e98b5
okaz <mikadukim@gmail.com>
2023-01-09 19:31:51 +0900

    投稿編集フォームを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get "posts/new" => "posts#new"
   post "posts/create" => "posts#create"
   get "posts/:id" => "posts#show"
+  get "posts/:id/edit" => "posts#edit"
 end
