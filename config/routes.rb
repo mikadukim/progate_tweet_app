317860ed09b2de410645899c85122cd4f133931c
okaz <mikadukim@gmail.com>
2023-01-09 18:12:11 +0900

    新規投稿機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "about" => "home#about"
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
   get "posts/:id" => "posts#show"
 end
