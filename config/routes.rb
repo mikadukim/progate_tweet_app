383c5b8e2eb825c7739670bc1f4abf2c96698278
okaz <mikadukim@gmail.com>
2023-01-09 17:48:05 +0900

    新規投稿の入力フォームを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "/" => "home#top"
   get "about" => "home#about"
   get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
 end
